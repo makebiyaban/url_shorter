9191759fa67e0b170e2fe0de41a056bec22fefad
mohamad abasi <mak47f@gmail.com>
2022-02-27 00:50:28 +0330

    Update __init__.py

--- url_app/__init__.py
+++ url_app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
 app1=Flask(__name__)
+db=SQLAlchemy(app1)
+migrate=Migrate(app1,db)
 app1.config.from_object(Config)
 
-from url_app import routes
-
+from url_app import routes, models
