2c43ee67d188aff855518f0b6a1ad69ae63378c2
makebiyaban <mak47f@gmail.com>
2022-03-01 23:22:14 +0330

    all login and hash extention

--- url_app/__init__.py
+++ url_app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_login import LoginManager
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
 app1=Flask(__name__)
+login=LoginManager(app1)
 app1.config.from_object(Config)
 db=SQLAlchemy(app1)
 migrate=Migrate(app1,db)
