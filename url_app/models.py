4f44339d2b09a059f808323b406b6c54caca7c75
makebiyaban <mak47f@gmail.com>
2022-03-02 08:04:34 +0330

    add longin user extention

--- url_app/models.py
+++ url_app/models.py
@@ -1,14 +1,18 @@
 from datetime import datetime
+import imp
 from xml.dom.pulldom import default_bufsize
 from url_app import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from app1 import login
 
-class User (db.Model):
+class User (UserMixin, db.Model):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(64),index=True,unique=True)
     email=db.Column(db.String(120),index=True,unique=True)
     password_hash=db.Column(db.String(128))
     posts=db.relationship('Post',backref='author',lazy='dynamic')
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -26,3 +30,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
