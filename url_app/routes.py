35ddfb7774b0bcb1bf405fd338f626c1714a35a8
makebiyaban <mak47f@gmail.com>
2022-03-03 21:15:45 +0330

    login

--- url_app/routes.py
+++ url_app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from url_app import app1
 from url_app.forms import LoginForm
+from flask_login import current_user, login_user
+from url_app.models import User
 
 @app1.route('/')
 def index():
@@ -22,7 +24,14 @@ def index():
 @app1.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        flash('login requested for user {}, remmber_me={}'.format(form.username.data, form.remmber_me.data))
-        return redirect('/')
+    if current_user.authenticated:
+        return redirect(url_for('/'))
+    if form.validate_on_submit():    
+        username=form.username.data
+        user=User.query.filter_by(username).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user,remember=form.remmber_me.data)
+        return redirect(url_for('/'))
     return render_template('login.html',title='sign in',form=form)
