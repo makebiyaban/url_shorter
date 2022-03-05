d72af53dc4a5cc80fa6dc5e2cdaf8a23204d83d4
makebiyaban <mak47f@gmail.com>
2022-03-05 09:35:11 +0330

    run app on mobile

--- url.py
+++ url.py
@@ -4,3 +4,7 @@ from url_app.models import User,Post
 @app1.shell_context_processor
 def make_shell_context():
     return {'db':db, 'User':User, 'Post':Post}
+    
+    
+if __name__ == "__main__" :
+	app1.run()
